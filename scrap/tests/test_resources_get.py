70a64d2eb47aa4dab1799951bacb5b4f1a985e3a
alfonso <@lfareiza1989>
2020-08-10 18:10:00 -0300

    tests

--- scrap/tests/test_resources_get.py
+++ scrap/tests/test_resources_get.py
@@ -1,4 +1,3 @@
-from scrap.main import NEWSPAPERS
 from scrap.resources import get
 
 TITLE = '[Videos] [Video] ¿Quién En Vivo | Video |  hizo   más ruido?\n Noche de (VIDEO) cacerolazos\t(VIDEO) '
@@ -9,6 +8,3 @@ URL = ''
 def test_text_without_removewords():
     text_cleaned = get.clean_text(TITLE)
     assert '[Videos]' not in text_cleaned
-
-if __name__ == '__main__':
-    print(NEWSPAPERS)
