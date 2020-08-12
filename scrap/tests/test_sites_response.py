7d404fed7c6ba0e686884a4efd44ba2533a9e055
alfonso <@lfareiza1989>
2020-08-11 23:16:02 -0300

    First test of my project

--- scrap/tests/test_sites_response.py
+++ scrap/tests/test_sites_response.py
@@ -5,27 +5,23 @@ from scrap.resources import get
 
 
 @pytest.mark.parametrize('urls', NEWSPAPERS)
-def test_response_200(urls):
-    response = requests.get(urls, timeout=TIMEOUT)
-    assert response.status_code == 200
+def test_sites_availables(urls):
+    request = requests.get(urls, timeout=TIMEOUT)
+    assert request.status_code == 200
 
 
 @pytest.mark.parametrize('urls_and_css_selectors', NEWSPAPERS)
 def test_set_of_news_with_css_selector(urls_and_css_selectors):
-    response = requests.get(urls_and_css_selectors, timeout=TIMEOUT)
+    request = requests.get(urls_and_css_selectors, timeout=TIMEOUT)
     css_selector = NEWSPAPERS.get(urls_and_css_selectors)
-    set_of_news = get.gettin_news(css_selector, response)
+    set_of_news = get.gettin_news(css_selector, request)
     assert len(set_of_news) > 0
 
 
 @pytest.mark.parametrize('urls', NEWSPAPERS)
 def test_domain_in_link_valid(urls):
-    response = requests.get(urls, timeout=TIMEOUT)
+    request = requests.get(urls, timeout=TIMEOUT)
     css_selector = NEWSPAPERS.get(urls)
-    set_of_news = get.gettin_news(css_selector, response)
+    set_of_news = get.gettin_news(css_selector, request)
     link = get.link_valid(set_of_news[0], urls)
     assert get.url_analyse(urls) in link
-
-
-if __name__ == '__main__':
-    print(NEWSPAPERS)
